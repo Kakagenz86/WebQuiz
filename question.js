73b8bbb5ff9bd8352f8a0999845d26188f7e142c
kakagenz <kakagenz86@gmail.com>
2023-11-16 15:17:16 +0800

    add web quiz

---
+++ question.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question:"Apa singkatan dari HTML?",
+        answer:"C. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Text Multiple Language",
+            "B. Hyper Type Multi Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Apa singkatan dari CSS?",
+        answer: "A. Cascading Style Sheet",
+        options: [
+            "A. Cascading Style Sheet",
+            "B. Cute Style Sheet",
+            "C. Computer Style Sheet",
+            "D. Codehal Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Bagaimana cara menyematkan CSS eksternal ke dalam halaman HTML?",
+        answer: "B. link rel='stylesheet' href='styles.css'",
+        options: [
+            "A. style src='styles.css'></style",
+            "B. link rel='stylesheet' href='styles.css'",
+            "C. css include='styles.css'></css",
+            "D. style href='styles.css'></style"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Apa yang dimaksud dengan 'box model' di CSS?",
+        answer: "C. Model yang mendefinisikan tata letak dan ukuran elemen-elemen HTML",
+        options: [
+            "A. Model tata letak elemen-elemen HTML",
+            "B. Model untuk mengelola file CSS",
+            "C. Model yang mendefinisikan tata letak dan ukuran elemen-elemen HTML",
+            "D. Model untuk mengelola elemen CSS"
+        ]
+    },
+    {
+        numb: 5,
+        question: " Apa kegunaan media query dalam CSS?",
+        answer: "D. Untuk mengatur tata letak dan tampilan halaman berdasarkan ukuran layar",
+        options: [
+            "A. Untuk menyematkan file media",
+            "B. Untuk membuat animasi",
+            "C. Untuk mengatur file css",
+            "D. Untuk mengatur tata letak dan tampilan halaman berdasarkan ukuran layar"
+        ]
+    },
+    {
+        numb: 6,
+        question: " Apa yang dimaksud dengan 'boolean' dalam pemrograman?",
+        answer: "B. Sebuah tipe data yang hanya dapat memiliki dua nilai: true atau false",
+        options: [
+            "A. Sebuah jenis data yang mewakili angka desimal",
+            "B. Sebuah tipe data yang hanya dapat memiliki dua nilai: true atau false",
+            "C. Sebuah tag HTML",
+            "D. Sebuah fungsi CSS",
+        ]
+    },
+    {
+        numb: 7,
+        question: " Bagaimana cara mendeklarasikan variabel di JavaScript?",
+        answer: "B. let myVar;",
+        options: [
+            "A. var myVar;",
+            "B. let myVar;",
+            "C. variable myVar;",
+            "D. let myVar()",
+        ]
+    },
+    {
+        numb: 8,
+        question: " Apa fungsi dari tag <div> di HTML?",
+        answer: "C. Mengelompokkan elemen-elemen HTML",
+        options: [
+            "A. Membuat teks berjalan",
+            "B. Menyisipkan gambar",
+            "C. Mengelompokkan elemen-elemen HTML",
+            "D. Membuat hyperlink",
+        ]
+    },
+    {
+        numb: 9,
+        question: " Apa fungsi dari properti 'position' di CSS?",
+        answer: "A. Mengatur tata letak elemen",
+        options: [
+            "A. Mengatur tata letak elemen",
+            "B. Mengubah warna latar belakang",
+            "C. Mengatur font teks",
+            "D. Mengubah ukuran elemen"
+        ]
+    },
+    {
+        numb: 10,
+        question: " Apa kegunaan media query dalam CSS?",
+        answer: "B. Bagaimana cara memberikan komentar di CSS?",
+        options: [
+            "A. -- Ini adalah komentar --",
+            "B. /* Ini adalah komentar */",
+            "C. // Ini adalah komentar",
+            "D. /!-- Ini adalah komentar --/"
+        ]
+    }
+]
